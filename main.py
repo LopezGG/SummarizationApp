773b4f6b6179d3ef8947f9a09739cc5bdbcf21f0
LopezGG <lopezgilsinia@gmail.com>
2021-03-19 16:39:20 +0530

    updating yml with package details

--- main.py
+++ main.py
@@ -22,26 +22,3 @@ class TextOut(BaseModel):
     # routes
 def root():
     return {"message":"Jesus please help me. O Lord I need you"}
-'''
-@testApp.post("/predict", response_model=TextOut, status_code=200)
-def get_prediction(payload: TextIn):
-    """
-    Gets a text and runs through T5, GPT2 and outputs summary
-    """
-
-    text = payload.text
-    minLen = payload.minLen
-    maxLen = payload.maxLen
-    t5Summary = predictT5(text,minLen,maxLen)
-    if not t5Summary:
-        raise HTTPException(status_code=400, detail="Unable to run T5")
-    summary = dict()
-    summary['t5'] = t5Summary
-    response_object = {"Summary": summary}
-    return response_object'''
-
-"""
-activate learning
-uvicorn main:testApp --reload
-
-"""
