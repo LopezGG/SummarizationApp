4363a0d27c6e986de50683b61bbf2ee58441b70d
LopezGG <agnelomaria@gmail.com>
2021-03-19 15:11:29 +0530

    fully functioning fastapi app

---
+++ main.py
@@ -0,0 +1,47 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from model import predictT5
+
+testApp = FastAPI() # create an app
+
+@testApp.get("/") # set root directory
+
+# pydantic models
+
+
+class TextIn(BaseModel):
+    text: str
+    minLen: int
+    maxLen: int
+
+class TextOut(BaseModel):
+    Summary: dict # this should not have () after dict
+
+    # routes
+def root():
+    return {"message":"Jesus please help me. O Lord I need you"}
+
+@testApp.post("/predict", response_model=TextOut, status_code=200)
+def get_prediction(payload: TextIn):
+    """
+    Gets a text and runs through T5, GPT2 and outputs summary
+    """
+
+    text = payload.text
+    minLen = payload.minLen
+    maxLen = payload.maxLen
+    t5Summary = predictT5(text,minLen,maxLen)
+    if not t5Summary:
+        raise HTTPException(status_code=400, detail="Unable to run T5")
+    summary = dict()
+    summary['t5'] = t5Summary
+    response_object = {"Summary": summary}
+    return response_object
+
+"""
+activate learning
+uvicorn main:testApp --reload
+
+"""
---
+++ model.py
@@ -0,0 +1,29 @@
+#backend/main
+import torch
+import json 
+from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
+#from pathlib import Path
+#BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+model = T5ForConditionalGeneration.from_pretrained('t5-large')
+tokenizer = T5Tokenizer.from_pretrained('t5-large')
+device = torch.device('cpu')
+
+def predictT5(text: str, minLen: int = 5,maxLen: int = 100):
+    """
+    takes in text  to generate summary
+    """
+    preprocess_text = text.strip().replace("\n","")
+    t5_prepared_Text = "summarize: "+preprocess_text
+    tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
+    # summmarize 
+    summary_ids = model.generate(tokenized_text,
+                                    num_beams=4,
+                                    no_repeat_ngram_size=2,
+                                    min_length=minLen,
+                                    max_length=maxLen,
+                                    early_stopping=True)
+    output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return output
+
+
